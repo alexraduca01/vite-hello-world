75659e7823f051915269305fc6b60a9462198469
alexraduca01 <alex.raduca01@icloud.com>
2023-11-08 15:22:56 +0100

    updated App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,7 +6,7 @@
 </template>
 
 <script>
-import AppTitle from './components/appTitle.vue';
+import AppTitle from './components/AppTitle.vue';
 import AppHero from './components/AppHero.vue';
 export default {
   components: { 
